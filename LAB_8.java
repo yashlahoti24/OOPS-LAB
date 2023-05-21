8b8c1ffd056b9703f87d5814c6d4bb01bbe8a273
yashlahoti24 <104621083+yashlahoti24@users.noreply.github.com>
2023-05-21 15:05:39 +0530

    Phone Book
    You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person. Use HashMap to implement it.The first line will have an integer denoting the number of entries in the phone book. Each entry consists of two lines: a name and the corresponding phone number.
    After these, there will be some queries. Each query will contain a person's name. Read the queries until end-of-file.
    Constraints:
    A person's name consists of only lower-case English letters and it may be in the format 'first-name last-name' or in the format 'first-name'. Each phone number has exactly 8 digits without any leading zeros.For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number.

--- LAB_8.java
+++ LAB_8.java
@@ -1,3 +1,9 @@
+// You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person. Use HashMap to implement it.The first line will have an integer denoting the number of entries in the phone book. Each entry consists of two lines: a name and the corresponding phone number.
+// After these, there will be some queries. Each query will contain a person's name. Read the queries until end-of-file.
+// Constraints:
+// A person's name consists of only lower-case English letters and it may be in the format 'first-name last-name' or in the format 'first-name'. Each phone number has exactly 8 digits without any leading zeros.For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number.
+
+
 import java.util.*;
 
 class PhoneBook {
