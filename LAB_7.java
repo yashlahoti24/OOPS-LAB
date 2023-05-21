27ae282e6c56bb8753459a84e8feb2d126c445ce
yashlahoti24 <104621083+yashlahoti24@users.noreply.github.com>
2023-05-21 15:04:42 +0530

    Power of a number
    You are required to compute the power of a number by implementing a calculator. Create a class My Calculator which consists of a single method long power (int, int). This method takes two integers n and p, as parameters and finds (n)p. If either or is negative, then the method must throw an exception which says " n or p should not be negative”. Also, if both and are zero, then the method must throw an exception which says "n or p should not be negative”.

--- LAB_7.java
+++ LAB_7.java
@@ -1,3 +1,5 @@
+// You are required to compute the power of a number by implementing a calculator. Create a class My Calculator which consists of a single method long power (int, int). This method takes two integers n and p, as parameters and finds (n)p. If either or is negative, then the method must throw an exception which says " n or p should not be negative”. Also, if both and are zero, then the method must throw an exception which says "n or p should not be negative”.
+
 import java.util.Scanner;
 
 // Custom exception class for handling negative inputs
