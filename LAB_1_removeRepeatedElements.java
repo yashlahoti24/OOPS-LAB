8d73d29519b9e0a896aa133e4014238ea5e1ee2d
yashlahoti24 <104621083+yashlahoti24@users.noreply.github.com>
2023-05-22 19:03:59 +0530

    removeRepeatedElements

--- LAB_1_removeRepeatedElements.java
+++ LAB_1_removeRepeatedElements.java
@@ -1,44 +1,49 @@
-// Question 1.1
-//import java.util.*;
+//// Question 1.1
 //
-//class ArrayProcessor {
-//    private int[] array;
+//import java.util.Arrays;
+//import java.util.Scanner;
 //
-//    public ArrayProcessor(int[] array) {
-//        this.array = array;
-//    }
+//class ArrayUtils {
+//    public static int[] removeDuplicates(int[] arr) {
+//        int n = arr.length;
+//        int[] uniqueArray = new int[n];
+//        int uniqueCount = 0;
 //
-//    public int[] removeDuplicates() {
-//        Set<Integer> uniqueElements = new LinkedHashSet<>();
-//        for (int element : array) {
-//            uniqueElements.add(element);
+//        for (int i = 0; i < n; i++) {
+//            boolean isDuplicate = false;
+//
+//            for (int j = 0; j < uniqueCount; j++) {
+//                if (arr[i] == uniqueArray[j]) {
+//                    isDuplicate = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!isDuplicate) {
+//                uniqueArray[uniqueCount] = arr[i];
+//                uniqueCount++;
+//            }
 //        }
 //
-//        int[] result = new int[uniqueElements.size()];
-//        int index = 0;
-//        for (int element : uniqueElements) {
-//            result[index++] = element;
-//        }
-//
-//        return result;
+//        return Arrays.copyOf(uniqueArray, uniqueCount);
 //    }
 //}
 //
-//public class LAB_1_removeRepeatedElements {
+//public class LAB_1 {
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //
-//        System.out.print("Enter the number of elements in the array: ");
+//        System.out.print("Enter the size of the array: ");
 //        int size = scanner.nextInt();
 //
-//        int[] array = new int[size];
+//        int[] arr = new int[size];
+//
 //        System.out.println("Enter the elements of the array:");
 //        for (int i = 0; i < size; i++) {
-//            array[i] = scanner.nextInt();
+//            arr[i] = scanner.nextInt();
 //        }
 //
-//        ArrayProcessor processor = new ArrayProcessor(array);
-//        int[] uniqueArray = processor.removeDuplicates();
+//        int[] uniqueArray = ArrayUtils.removeDuplicates(arr);
 //
 //        System.out.println("Array with duplicate elements removed:");
 //        for (int element : uniqueArray) {
@@ -47,70 +52,41 @@
 //    }
 //}
 
-//Question 1.2
-import java.util.*;
 
-class ArrayProcessor {
-    private int[] array1;
-    private int[] array2;
+// Question 1.2
+import java.util.Scanner;
 
-    public ArrayProcessor(int[] array1, int[] array2) {
-        this.array1 = array1;
-        this.array2 = array2;
-    }
-
-    public int[] findCommonElements() {
-        Set<Integer> commonElements = new HashSet<>();
-        for (int element : array1) {
-            for (int value : array2) {
-                if (element == value) {
-                    commonElements.add(element);
-                    break;
-                }
-            }
-        }
-
-        int[] result = new int[commonElements.size()];
-        int index = 0;
-        for (int element : commonElements) {
-            result[index++] = element;
-        }
-
-        return result;
-    }
-}
-
-public class LAB_1_removeRepeatedElements {
+public class LAB_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input for the first array
-        System.out.print("Enter the number of elements in the first array: ");
-        int size1 = scanner.nextInt();
-
-        int[] array1 = new int[size1];
-        System.out.println("Enter the elements of the first array:");
-        for (int i = 0; i < size1; i++) {
-            array1[i] = scanner.nextInt();
+        // Input array A
+        System.out.print("Enter the size of Array A: ");
+        int sizeA = scanner.nextInt();
+        int[] arrayA = new int[sizeA];
+        System.out.println("Enter the elements of Array A:");
+        for (int i = 0; i < sizeA; i++) {
+            arrayA[i] = scanner.nextInt();
         }
 
-        // Input for the second array
-        System.out.print("Enter the number of elements in the second array: ");
-        int size2 = scanner.nextInt();
-
-        int[] array2 = new int[size2];
-        System.out.println("Enter the elements of the second array:");
-        for (int i = 0; i < size2; i++) {
-            array2[i] = scanner.nextInt();
+        // Input array B
+        System.out.print("Enter the size of Array B: ");
+        int sizeB = scanner.nextInt();
+        int[] arrayB = new int[sizeB];
+        System.out.println("Enter the elements of Array B:");
+        for (int i = 0; i < sizeB; i++) {
+            arrayB[i] = scanner.nextInt();
         }
 
-        ArrayProcessor processor = new ArrayProcessor(array1, array2);
-        int[] commonElements = processor.findCommonElements();
-
-        // Display common elements
-        System.out.println("Common elements between the two arrays:");
-        for (int element : commonElements) {
-            System.out.print(element + " ");
+        // Find common elements
+        System.out.println("Common elements between Array A and Array B:");
+        for (int i = 0; i < sizeA; i++) {
+            for (int j = 0; j < sizeB; j++) {
+                if (arrayA[i] == arrayB[j]) {
+                    System.out.println(arrayA[i]);
+                    break;
+                }
+            }
         }
     }
 }
